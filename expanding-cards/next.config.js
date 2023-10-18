d906611effdfc6e059d3e6fb1b7e67f8f7573d95
Andres Felipe Rodriguez Fuentes <ing_andresrodriguez@outlook.com>
2023-10-18 17:24:11 -0500

    fix (Cards): bug fix images

--- expanding-cards/next.config.js
+++ expanding-cards/next.config.js
@@ -1,6 +1,10 @@
 /** @type {import('next').NextConfig} */
 const nextConfig = {
-  output: 'export'
+  output: 'export',
+  images: {
+    unoptimized: true,
+    formats: ['image/webp'],
+  }
 }
 
 module.exports = nextConfig
--- expanding-cards/src/data/index.ts
+++ expanding-cards/src/data/index.ts
@@ -2,23 +2,23 @@ import { Image } from "@/interfaces";
 
 export const superHeros: Image[] = [
   {
-    src: '/assets/images/superheroes/black-panther-marvel-characters.webp',
+    src: './assets/images/superheroes/black-panther-marvel-characters.webp',
     alt: 'Black Panther'
   },
   {
-    src: '/assets/images/superheroes/captain-america-marvel-characters.webp',
+    src: './assets/images/superheroes/captain-america-marvel-characters.webp',
     alt: 'Captain America'
   },
   {
-    src: '/assets/images/superheroes/iron-man-marvel-character.webp',
+    src: './assets/images/superheroes/iron-man-marvel-character.webp',
     alt: 'Iron Man'
   },
   {
-    src: '/assets/images/superheroes/killmonger-marvel-character.webp',
+    src: './assets/images/superheroes/killmonger-marvel-character.webp',
     alt: 'Killmonger'
   },
   {
-    src: '/assets/images/superheroes/spider-man-marvel-character.webp',
+    src: './assets/images/superheroes/spider-man-marvel-character.webp',
     alt: 'Spider Man'
   }
 ]
